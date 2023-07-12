a018e285e626b2c15c761571b5635c3c25c04f6d
dariofusco <fdario@hotmail.it>
2023-07-12 15:38:39 +0200

    aggiunge funzioni  generatesquare addtoggle

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
 document.getElementById("myBtn").addEventListener("click", function () {
-
-    const div = document.querySelector("div.row")
-
-    for (let i = 1; i <= 100; i++) {
-        const singleSquare = `<div class="${i} border border-1 square">${i}</div>`
-        div.innerHTML += singleSquare
-    }
+    
+    generateSquare ()
 
     const numRandomArray = []
+
     for (let i = 1; i <= 16; i++) {
         const numRandom = Math.round(Math.random() * 99) + 1;
         console.log(numRandom)
@@ -19,13 +15,29 @@ document.getElementById("myBtn").addEventListener("click", function () {
         console.log(bomb)
     }
 
+    addToggle()
+
+})
+
+function generateSquare () {
+    const div = document.querySelector("div.row")
+
+    for (let i = 1; i <= 100; i++) {
+        const singleSquare = `<div class="${i} border border-1 square">${i}</div>`
+        div.innerHTML += singleSquare
+    }
+
+}
+
+function addToggle () {
     const squareList = document.querySelectorAll(".square")
 
     for (let i = 0; i < squareList.length; i++) {
+        
         squareList[i].addEventListener("click", function () {
             squareList[i].classList.toggle("bg-success")
             console.log(squareList[i].innerHTML)
         })
+        
     }
-
-})
+}
